19ac738d2995f208de7a4d6f9044ebf4c0a8e3d4
Michele <michele47801@gmail.com>
2025-02-01 04:43:39 +0000

    Snowy structure

--- snowy.py
+++ snowy.py
@@ -1,1 +1,30 @@
-print("hello world")
+import asyncio
+import json
+import os
+import platform
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+STATE = "development"
+
+load_dotenv(f"schema/.env.{STATE}")
+
+TOKEN = os.getenv("TOKEN")
+MYSQL = os.getenv("MYSQL")
+
+with open(f"schema/design.{STATE}.json", "r") as file:
+    schema = json.load(file)
+
+bot = commands.Bot(command_prefix=schema["command_prefix"], intents=discord.Intents.all())
+
+print(f"Snowy {STATE} Robot Running")
+print(f"Running at Python {platform.python_version()}v, "
+      f"Discord.py {discord.__version__}v - {platform.system()} {platform.release()} ({os.name})\n")
+
+
+async def main():
+    await bot.start(TOKEN)
+
+asyncio.run(main())
