dc22e0f664a3782d9b2d28b00f9e34143ce910af
Michele <michele47801@gmail.com>
2025-02-02 20:03:34 +0000

    New uptime retrieval function also ask for format and timezone

--- structure/helper.py
+++ structure/helper.py
@@ -2,7 +2,21 @@ from datetime import datetime
 import pytz
 
 
-def get_time():
-    london_tz = pytz.timezone('Europe/London')
-    london_time = datetime.now(london_tz)
-    return london_time.strftime('%d %B %Y %H:%M %Z')
+def get_time(format: str = "%d %B %Y %H:%M %Z", timezone: str = "Europe/London"):
+    tz = pytz.timezone(timezone)
+    time = datetime.now(tz)
+    return time.strftime(format)
+
+
+def get_uptime(bot, format: str = "%d %B %Y %H:%M %Z", timezone: str = "Europe/London"):
+    master_cog = bot.get_cog("MasterCog")
+    if not master_cog:
+        return "MasterCog not loaded."
+
+    start_time = master_cog.get_uptime()
+    tz = pytz.timezone(timezone)
+    localized_start = tz.localize(start_time)
+
+    return localized_start.strftime(format)
+
+
