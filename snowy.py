6d4a1b41c20eb265b76ba8adf19ed74b043090ae
Michele <michele47801@gmail.com>
2025-02-02 19:13:29 +0000

    Small adjustments

--- snowy.py
+++ snowy.py
@@ -20,18 +20,18 @@ with open(f"schema/design.{STATE}.json", "r") as file:
 
 bot = commands.Bot(command_prefix=schema["command_prefix"], intents=discord.Intents.all())
 
-print(f"Snowy {STATE} Robot Running")
+print(f"Snowy {STATE} Robot")
 print(f"Running at Python {platform.python_version()}v, "
       f"Discord.py {discord.__version__}v - {platform.system()} {platform.release()} ({os.name})\n")
 
 
 async def load():
-    for file in Path("structure").glob("*.py"):
-        if file.stem != "__init__":
+    for extension in Path("structure").glob("*.py"):
+        if extension.stem != "__init__":
             try:
-                await bot.load_extension(f"{"structure"}.{file.stem}")
+                await bot.load_extension(f"{"structure"}.{extension.stem}")
             except commands.NoEntryPointError:
-                print(f"Skipped loading extension '{file.stem}' as it does not have a 'setup' function")
+                print(f"Skipped loading extension '{extension.stem}' as it does not have a 'setup' function")
 
 
 async def main():
