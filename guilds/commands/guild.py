c0940a8ddc80572dce9d299d0f58e1ac7fe3baa7
michele <michelegje@gmail.com>
2025-07-29 03:41:25 +0100

    Remove debugger

--- guilds/commands/guild.py
+++ guilds/commands/guild.py
@@ -30,39 +30,36 @@ class GuildCommand(commands.Cog):
         description="Show guild information"
     )
     async def _guild_info(self, ctx, guild_id: int = None):
-        try:
-            create_or_update_guild(guild_id)
-            guild_db = get_guild_by_id(guild_id)
+        create_or_update_guild(guild_id)
+        guild_db = get_guild_by_id(guild_id)
 
-            if not guild_db:
-                await ctx.send(f"No guild matching **{guild_id}** found!")
-                return
+        if not guild_db:
+            await ctx.send(f"No guild matching **{guild_id}** found!")
+            return
 
-            guild = self.bot.get_guild(guild_id)
+        guild = self.bot.get_guild(guild_id)
 
-            description = (
-                f"**ᴀᴅᴅᴇᴅ ᴀᴛ**: **{get_formatted_time(guild_db.added_at, format="%d/%m/%y %H:%M %Z")}**\n"
-                f"**ᴀᴄᴛɪᴠᴇ**: {'✅' if guild_db.is_active else '❎'}\n"
+        description = (
+            f"**ᴀᴅᴅᴇᴅ ᴀᴛ**: **{get_formatted_time(guild_db.added_at, format="%d/%m/%y %H:%M %Z")}**\n"
+            f"**ᴀᴄᴛɪᴠᴇ**: {'✅' if guild_db.is_active else '❎'}\n"
+        )
+
+        if not guild_db.is_active:
+            description += (
+                f"**ʀᴇᴍᴏᴠᴇᴅ ᴀᴛ**: {get_formatted_time(guild_db.removed_at, format="%d/%m/%y %H:%M %Z")}\n"
             )
 
-            if not guild_db.is_active:
-                description += (
-                    f"**ʀᴇᴍᴏᴠᴇᴅ ᴀᴛ**: {get_formatted_time(guild_db.removed_at, format="%d/%m/%y %H:%M %Z")}\n"
-                )
+        embed = discord.Embed(
+            title=f"ɢᴜɪʟᴅ ɪɴꜰᴏ ꜰᴏʀ {guild.name}",
+            description=description,
+            color=0x393A41,
+            timestamp=datetime.utcnow()
+        )
 
-            embed = discord.Embed(
-                title=f"ɢᴜɪʟᴅ ɪɴꜰᴏ ꜰᴏʀ {guild.name}",
-                description=description,
-                color=0x393A41,
-                timestamp=datetime.utcnow()
-            )
+        avatar_url = guild.icon.url if guild.icon else "https://cdn.discordapp.com/embed/avatars/0.png"
+        embed.set_thumbnail(url=avatar_url)
 
-            avatar_url = guild.icon.url if guild.icon else "https://cdn.discordapp.com/embed/avatars/0.png"
-            embed.set_thumbnail(url=avatar_url)
-
-            await ctx.send(embed=embed)
-        except Exception as e:
-            print(e)
+        await ctx.send(embed=embed)
 
 
 async def setup(bot):
