456c5d7e9a0513ad5df2d45ffc22661aa63e57b0
michele <michelegje@gmail.com>
2025-08-05 05:28:29 +0100

    Improved the permission model

--- backend/permissions/models.py
+++ backend/permissions/models.py
@@ -1,15 +1,19 @@
-from sqlalchemy import Column, BigInteger, String, Boolean, ForeignKey, ARRAY
+from sqlalchemy import Column, BigInteger, String, Boolean, ForeignKey, ARRAY, DateTime
 from sqlalchemy.orm import relationship
 
 from backend.core.database import Base
+from backend.core.helper import get_utc_now
 
 
 class Permission(Base):
     __tablename__ = "permissions"
 
-    guild_id = Column(BigInteger, ForeignKey("guilds.guild_id"), primary_key=True)
+    command_id = Column(BigInteger, primary_key=True)
+    guild_id = Column(BigInteger, ForeignKey("guilds.guild_id"), index=True)
     command_name = Column(String(64), nullable=False)
-    roles = Column(ARRAY(BigInteger), default=list)
+    allowed_roles = Column(ARRAY(BigInteger), default=list)
+    added_at = Column(DateTime, default=get_utc_now())
+    is_admin = Column(Boolean, default=True)
     is_enabled = Column(Boolean, default=True)
 
     guild = relationship("Guild", back_populates="permission")
