3a3702f466f32600bda3167129d9a88b73d04075
michele <michelegje@gmail.com>
2025-08-05 01:35:21 +0100

    Implement Pagination for lists embed

---
+++ backend/core/pagination.py
@@ -0,0 +1,74 @@
+from discord import Embed, Interaction, ButtonStyle
+from discord.ui import View, Button
+
+from backend.core.helper import get_utc_now
+
+
+def chunk_lines(lines: list[str], size: int) -> list[str]:
+    return [
+        "\n".join(lines[i:i + size])
+        for i in range(0, len(lines), size)
+    ]
+
+
+class Pagination(View):
+    def __init__(
+            self,
+            title: str,
+            lines: list[str],
+            lines_per_page: int,
+            author_id: int
+    ):
+        super().__init__(timeout=60)
+        self.title = title
+        self.pages = chunk_lines(lines, lines_per_page)
+        self.page = 0
+        self.author_id = author_id
+        self.build_buttons()
+
+    def create_embed(self) -> Embed:
+        return Embed(
+            title=self.title,
+            description=self.pages[self.page],
+            color=0x2F3136,
+            timestamp=get_utc_now()
+        ).set_footer(text=f"{self.page + 1}/{len(self.pages)}")
+
+    def build_buttons(self):
+        self.clear_items()
+
+        if self.page > 0:
+            self.add_item(Button(label="⏮️", style=ButtonStyle.grey, custom_id="first"))
+
+        self.add_item(Button(label="◀️", style=ButtonStyle.blurple, custom_id="prev"))
+        self.add_item(Button(label="▶️", style=ButtonStyle.blurple, custom_id="next"))
+
+        if self.page < len(self.pages) - 1:
+            self.add_item(Button(label="⏭️", style=ButtonStyle.grey, custom_id="last"))
+
+    async def interaction_check(self, interaction: Interaction) -> bool:
+        if interaction.user.id != self.author_id:
+            await interaction.response.send_message("You are not allowed to use this!", ephemeral=True)
+            return False
+
+        if not interaction.response.is_done():
+            await interaction.response.defer()
+
+        await self.interaction_handler(interaction)
+        return True
+
+    async def interaction_handler(self, interaction: Interaction):
+        match interaction.data["custom_id"]:
+            case "first":
+                self.page = 0
+            case "prev":
+                if self.page > 0:
+                    self.page -= 1
+            case "next":
+                if self.page < len(self.pages) - 1:
+                    self.page += 1
+            case "last":
+                self.page = len(self.pages) - 1
+
+        self.build_buttons()
+        await interaction.message.edit(embed=self.create_embed(), view=self)
