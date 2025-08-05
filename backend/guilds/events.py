cbe3c36d840e973d569a83ca1900d3addf908d01
michele <michelegje@gmail.com>
2025-08-05 01:52:29 +0100

    Fixes

--- backend/guilds/events.py
+++ backend/guilds/events.py
@@ -5,17 +5,30 @@ from backend.guilds.manager import create_or_update_guild
 
 
 class GuildEvents(commands.Cog):
-    def __init__(self, bot): self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        print(f"New guild named {guild.name} has added! #{guild.id}")
-        create_or_update_guild(self.bot, guild.id, added_at=get_utc_now(), is_active=True)
+        print(f"New guild named {guild.name} has been added! #{guild.id}")
+
+        create_or_update_guild(
+            self.bot,
+            guild.id,
+            added_at=get_utc_now(),
+            is_active=True
+        )
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        print(f"A guild named {guild.name} left! #{guild.id}")
-        create_or_update_guild(self.bot, guild.id, removed_at=get_utc_now(), is_active=False)
+        print(f"A guild named {guild.name} has left! #{guild.id}")
+
+        create_or_update_guild(
+            self.bot,
+            guild.id,
+            removed_at=get_utc_now(),
+            is_active=False
+        )
 
 
 async def setup(bot):
