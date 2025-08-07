d7eadd68afc8f9834deed918415cfc0e844715bf
michele <michelegje@gmail.com>
2025-08-07 05:39:10 +0100

    Help command with categories

---
+++ backend/guilds/commands/help.py
@@ -0,0 +1,78 @@
+from discord.ext import commands
+
+from backend.configs.commands.punishment_admin import PunishmentAdminCommand
+from backend.core.helper import get_commands_help_messages
+from backend.core.pagination import Pagination
+from backend.permissions.commands.permission import PermissionCommand
+from backend.permissions.enforce import has_permission
+from backend.punishments.commands.ban import BanCommand
+from backend.punishments.commands.kick import KickCommand
+from backend.punishments.commands.mute import MuteCommand
+from backend.punishments.commands.punishment import PunishmentCommand
+from backend.punishments.commands.warn import WarnCommand
+
+
+class HelpCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @has_permission()
+    @commands.group(
+        name="help",
+        invoke_without_command=True
+    )
+    async def _help(self, ctx):
+        view = Pagination(
+            "ʜᴇʟᴘ ѕᴜʙᴄᴏᴍᴍᴀɴᴅѕ",
+            get_commands_help_messages(
+                self.bot,
+                [HelpCommand],
+                ctx.author.guild_permissions.administrator
+            ),
+            3,
+            ctx.author.id
+        )
+
+        await ctx.send(embed=view.create_embed(), view=view)
+
+    @has_permission()
+    @_help.command(name="moderation")
+    async def _help_moderation(self, ctx):
+        """
+        Get moderation related commands listing
+        """
+        view = Pagination(
+            "ᴍᴏᴅᴇʀᴀᴛɪᴏɴ ѕᴜʙᴄᴏᴍᴍᴀɴᴅѕ",
+            get_commands_help_messages(
+                self.bot,
+                [BanCommand, KickCommand, MuteCommand, WarnCommand, PunishmentCommand],
+                ctx.author.guild_permissions.administrator
+            ),
+            3,
+            ctx.author.id
+        )
+
+        await ctx.send(embed=view.create_embed(), view=view)
+
+    @has_permission()
+    @_help.command(name="management")
+    async def _help_management(self, ctx):
+        """
+        Get management related commands listing
+        """
+        view = Pagination(
+            "ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ѕᴜʙᴄᴏᴍᴍᴀɴᴅѕ",
+            get_commands_help_messages(
+                self.bot,
+                [PunishmentAdminCommand, PermissionCommand],
+                ctx.author.guild_permissions.administrator
+            ),
+            3,
+            ctx.author.id
+        )
+
+        await ctx.send(embed=view.create_embed(), view=view)
+
+
+async def setup(bot):
+    await bot.add_cog(HelpCommand(bot))
