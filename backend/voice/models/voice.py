9b4849b7d2b6ca89a8dc5744047ab57c596880ad
michele <michelegje@gmail.com>
2025-08-17 20:54:09 +0100

    Updated columns & values

--- backend/voice/models/voice.py
+++ backend/voice/models/voice.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, ForeignKey, ARRAY, Boolean, DateTime
+from sqlalchemy import Column, BigInteger, ForeignKey, Boolean, DateTime
 
 from backend.core.database import Base
 from backend.core.helper import get_time_now
@@ -10,7 +10,6 @@ class Voice(Base):
     channel_id = Column(BigInteger, primary_key=True)
     guild_id = Column(BigInteger, ForeignKey("guilds.guild_id"), index=True)
     owner_id = Column(BigInteger, nullable=False, index=True)
-    allowed_user_ids = Column(ARRAY(BigInteger), default=list)
     is_temporary = Column(Boolean, default=True, index=True)
     created_at = Column(DateTime, default=get_time_now())
     is_deleted = Column(Boolean, default=False, index=True)
--- backend/voice/models/voice_config.py
+++ backend/voice/models/voice_config.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, ForeignKey, DateTime, ARRAY, Integer
+from sqlalchemy import Column, BigInteger, ForeignKey, DateTime, ARRAY
 from sqlalchemy.dialects.postgresql import JSONB
 
 from backend.core.database import Base
@@ -6,19 +6,17 @@ from backend.core.helper import get_time_now
 
 
 class VoiceConfig(Base):
-    __tablename__ = "voice_configs"
+    __tablename__ = "voice_config"
 
     guild_id = Column(BigInteger, ForeignKey("guilds.guild_id", ondelete="CASCADE"), primary_key=True)
     category_id = Column(BigInteger, nullable=True)
     join_channel_id = Column(BigInteger, nullable=True)
-    command_channel_id = Column(BigInteger, nullable=True)
     embed = Column(JSONB,
                    default=lambda: {
                        "title": "Voice Channel Menu",
-                       "description": "No description",
+                       "description": "Use the buttons below to manage your channel",
                    },
                    nullable=True)
-    auto_delete_after = Column(Integer, default=15)
     staff_role_ids = Column(ARRAY(BigInteger), default=list)
     logging_channel_id = Column(BigInteger, nullable=True)
     updated_at = Column(DateTime, default=get_time_now(), onupdate=get_time_now())
